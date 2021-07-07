a1e71fccb79b67f4b90e7e1abd210c9fd43a14f2
Javier Olmedo <j.javierolmedo@gmail.com>
2021-07-08 00:19:31 +0200

    test

--- install.sh
+++ install.sh
@@ -42,6 +42,12 @@ installpackages(){
     sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y
     echo "${YELLOW}[+] Install Polybar packages${NC}"
     sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
+    echo "${YELLOW}[+] Install Picom packages${NC}"
+    sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y
+    echo "${YELLOW}[+] Install Ninja${NC}"
+    sudo apt install ninja -y
+    echo "${YELLOW}[+] Install Rofi${NC}"
+    sudo apt install rofi -y
     echo "${GREEN}[+] Install Packages --> Done!${NC}"
 }
 
@@ -62,6 +68,23 @@ installgits(){
     sudo make install
     echo "${YELLOW}[+] Install BSPWM from APT${NC}"
     sudo apt install bspwm -y
+    echo "${YELLOW}[+] Install Polybar${NC}"
+    cd ~/.tmp
+    git clone --recursive https://github.com/polybar/polybar
+    cd ~/.tmp/polybar
+    mkdir build
+    cd ~/.tmp/polybar/build/
+    cmake ..
+    make -j$(nproc)
+    sudo make install
+    echo "${YELLOW}[+] Install Picom${NC}"
+    cd ~/.tmp
+    git clone https://github.com/ibhagwan/picom.git
+    cd picom/
+    git submodule update --init --recursive
+    meson --buildtype=release . build
+    ninja -C build
+    sudo ninja -C build install
     echo "${GREEN}[+] Install Gits --> Done!${NC}"
     cd
 }
@@ -85,10 +108,11 @@ configfiles(){
 # Main
 main() {
     fullsystemupdate
-    #makefolders
-    #installpackages
-    #installgits
-    #configfiles
+    makefolders
+    installpackages
+    installgits
+    configfiles
+    sudo reboot
 }
 
 main
