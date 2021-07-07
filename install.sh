96a4ffce19590e37e62f75a28464b8590f3211b8
Javier Olmedo <j.javierolmedo@gmail.com>
2021-07-07 20:36:43 +0200

    test

--- install.sh
+++ install.sh
@@ -16,9 +16,16 @@ fullsystemupdate(){
     echo "${GREEN}[+] Updating system --> Done!${NC}"
 }
 
+installpackages(){
+    echo "${GREEN}-----------------------[Install Packages]-----------------------${NC}"
+    apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y
+    echo "${GREEN}[+] Install Packages --> Done!${NC}"
+}
+
 # Main
 main() {
     fullsystemupdate
+    installpackages
 }
 
 main
